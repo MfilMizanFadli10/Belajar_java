629350609f9a1d48581c3f9e88e0f0ea021dba85
MfilMizanFadli10 <ponselpagaruyuang36@gmail.com>
2025-02-05 15:03:03 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,11 @@
+public class Latihan1 {
+    public static void main(String[] args) {
+        String nama = "mizan";
+        String kelas = " X PPLG 2";
+        double B_badan = 60.00;
+        System.out.println("Nama saya : " +nama);
+        System.err.println("kelas : " +kelas);
+        System.err.print("Berat Badan : ");
+        System.out.println(B_badan);
+    }
+}
